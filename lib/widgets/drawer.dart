c377cf4b37a7274ebbf9ecab26e4269c04c1ea51
SaintBanana <thatbanana@duck.com>
2025-01-11 15:11:48 +0330

    fix: Update version display format in DrawerWidget

--- lib/widgets/drawer.dart
+++ lib/widgets/drawer.dart
@@ -116,7 +116,7 @@ class DrawerWidget extends StatelessWidget {
           Padding(
             padding: const EdgeInsets.all(8.0),
             child: Text(
-              "${cache.get("version")} (${cache.get("coreVersion")})", 
+              "v${cache.get("version")} (${cache.get("coreVersion")})", 
               style: TextStyle(
                 color: themeColors.secondaryTextColor,
                 fontSize: 11,
